37abf683a146d0db9a03966c413bb0b47b2f35e3
H3C70RdelAn9el <H3C70R@H3C70Rs-MBP.local>
2017-10-04 10:56:58 -0700

    Setup app for heroku fixes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,11 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI ||  'mongodb://localhost:27017/TodoApp');
-
+//mongoose.connect("mongodb://<Hector>:<12345>@ds161304.mlab.com:61304/safe-sierra-79150");
 //here we export the module!
+let db = {
+  localhost: 'mongodb://localhost:27017/TodoApp',
+  mlab: 'mongodb://<Hector>:<12345>@ds161304.mlab.com:61304/safe-sierra-79150'
+};
+mongoose.connect( db.localhost || db.mlab);
 module.exports = {mongoose};
