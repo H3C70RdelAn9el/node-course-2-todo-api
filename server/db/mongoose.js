6efaa8601283912007c11089fba240e9a3cbcde4
H3C70RdelAn9el <H3C70R@H3C70Rs-MBP.local>
2017-10-04 11:06:46 -0700

    Fixing!2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -10,5 +10,5 @@ mongoose.connect(process.env.MONGODB_URI ||  'mongodb://localhost:27017/TodoApp'
 //   localhost: 'mongodb://localhost:27017/TodoApp',
 //   mlab: 'mongodb://<Hector>:<12345>@ds161304.mlab.com:61304/safe-sierra-79150'
 // };
-mongoose.connect( db.localhost || db.mlab);
+// mongoose.connect( db.localhost || db.mlab);
 module.exports = {mongoose};
