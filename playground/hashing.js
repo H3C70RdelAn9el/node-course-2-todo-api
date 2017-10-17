93c4db7a3206be9f810e7f425835ffde6dda4ae4
H3C70RdelAn9el <H3C70R@H3C70Rs-MBP.local>
2017-10-17 12:19:20 -0700

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,32 @@
 /* jshint esversion: 6 */
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+//generate a SALT
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
 
-var decoded  = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+var hashedPassword = '$2a$10$0HRDSVHLVxa9IZo5lLrLreHkqJELCu4SyvbTnjP6gh48E2FI0qK/G';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+// var data = {
+//     id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded  = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 //jwt.verify
 
