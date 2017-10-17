f9403e5306e8da2e7d10db121bcbfde941f6c1ba
H3C70RdelAn9el <H3C70R@H3C70Rs-MBP.local>
2017-10-17 10:00:32 -0700

    Add GET /users/me and authentication middleware

---
+++ server/middleware/authenticate.js
@@ -0,0 +1,22 @@
+/* jshint esversion: 6 */
+//setting up middleware to authtenticate and private routes
+var {User} = require('./../models/user');
+
+var authenticate = (req, res, next) => {
+    var token = req.header('x-auth');
+
+    //making the model method to users
+    User.findByToken(token).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+
+        req.user = (user);
+        req.token = token;
+        next();
+    }).catch((e) => {
+        res.status(401).send();
+    });
+};
+
+module.exports = {authenticate};
--- server/models/user.js
+++ server/models/user.js
@@ -53,6 +53,26 @@ UserSchema.methods.generateAuthToken = function () {
     });
 };
 
+UserSchema.statics.findByToken = function(token) {
+        var User = this;
+        var decoded; //storing JWT values
+
+        try {
+            decoded = jwt.verify(token, 'abc123');
+        } catch (e) {
+            // return new Promise((resolve, reject) => {
+            //     reject();
+            // });
+            return Promise.reject();    //easier to use
+        }
+
+        return User.findOne({
+            '_id': decoded._id,
+            'tokens.token': token,
+            'tokens.access': 'auth'
+        });
+};
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
