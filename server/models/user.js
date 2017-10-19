ed413de6d841ad45a15c01203005566be7fe69ee
H3C70RdelAn9el <H3C70R@H3C70Rs-MBP.local>
2017-10-19 10:42:56 -0700

    Add tests for GET /users/me and POST /users

--- server/models/user.js
+++ server/models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
-const bcrypt = require('bcryptjs');
+// const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
     email: {
@@ -73,21 +73,21 @@ UserSchema.statics.findByToken = function(token) {
         });
     };
 
-UserSchema.pre('save', function (next) {
-    var user = this;
-
-    if (user.isModified('password')) {
-        // user.password
-        bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(user.password, salt, (err, hash) => {
-                user.password = hash;
-                next();
-            });
-        });
-    } else {
-        next();
-    }
-});
+// UserSchema.pre('save', function (next) {
+//     var user = this;
+//
+//     if (user.isModified('password')) {
+//         // user.password
+//         bcrypt.genSalt(10, (err, salt) => {
+//             bcrypt.hash(user.password, salt, (err, hash) => {
+//                 user.password = hash;
+//                 next();
+//             });
+//         });
+//     } else {
+//         next();
+//     }
+// });
 
 var User = mongoose.model('User', UserSchema);
 
