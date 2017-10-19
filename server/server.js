23ce51f69f870db2ff63a4690fefbd1dd6da3ec7
H3C70RdelAn9el <H3C70R@H3C70Rs-MBP.local>
2017-10-19 13:10:27 -0700

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -148,7 +148,14 @@ app.post('/users/login',(req, res) => {
         res.status(400).send();
     });
 });
-
+//POST /users/logout
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.status(400).send();
+    });
+});
 
 
 app.listen(port, () => {
